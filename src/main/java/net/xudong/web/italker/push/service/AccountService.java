9ff23a5ce0983f3ce732bfcdaab3e73c26c6f66c
sunxudong <sunxudong@yanhangtec.com>
2017-12-27 17:06:09 +0800

    完成第一个Restful接口

---
+++ src/main/java/net/xudong/web/italker/push/service/AccountService.java
@@ -0,0 +1,36 @@
+package net.xudong.web.italker.push.service;
+
+import net.xudong.web.italker.push.bean.User;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author xudongsun
+ * <p>
+ * 1.定义访问路径
+ */
+//实际访问路径 127.0.0.1/api/account/...
+@Path("/account")
+public class AccountService {
+
+    //127.0.0.1/api/account/login
+    @GET
+    @Path("/login")
+    public String get() {
+        return "You get the login by get.";
+    }
+
+    @POST
+    @Path("/login")
+    //指定请求与返回的相应体为JSON
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public User post() {
+        User user = new User();
+        user.setName("admin");
+        user.setAge(16);
+        return user;
+    }
+
+}
